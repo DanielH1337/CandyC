d0b040a08f35b12fbc310a4cdce6b46cd4f01d04
DanielH1337 <62145497+DanielH1337@users.noreply.github.com>
2020-12-13 18:38:05 +0200

    Score yli 1000 scenevaihto

--- Assets/Scripts/ScoreScript.cs
+++ Assets/Scripts/ScoreScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ScoreScript : MonoBehaviour
 
@@ -19,9 +20,18 @@ public class ScoreScript : MonoBehaviour
     void Update()
     {
         scoreText.text = "" + score;
+        if (score >= 1000)
+        {
+            LoadNextLevel();
+        }
+        
     }
     public void IncreaseScore(int amountToIncrease)
     {
         score += amountToIncrease;
+    }
+    public void LoadNextLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
     }
 }
